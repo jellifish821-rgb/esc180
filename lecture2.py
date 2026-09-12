7fee622f282e244ae7e7fc1974f5364f0a690222
jellifish821-rgb <jellifish821@gmail.com>
2026-09-12 16:11:39 -0400

    Initial commit

---
+++ lecture2.py
@@ -0,0 +1,12 @@
+# calc perimeter of a square
+side_length = 6
+perimeter = side_length * 4
+
+print(perimeter)
+
+side_length = 4
+
+#still prints 24 as te value of perimeter has not been changed
+print(perimeter)
+
+#swap values
